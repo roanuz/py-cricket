1cb08f9d660a0a5adf3c40af491810f0e76055f1
rajeev r <rajeev r>
2016-04-13 15:51:35 +0530

    module are grouped as package

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name = 'pycricket',
-    version = '0.1.1',
-    description = 'Roanuz Cricket API Library',
-    author = 'Rajeev Raju',
-    author_email = 'rajev.r@roanuz.com',
-    url = 'https://github.com/Rajeev69/pyCricket', 
-    py_modules=['pycricket'],
+    name = 'py-cricket',
+    version = '1.0.1',
+    description = 'Easy to install and simple way to access all Roanuz Cricket APIs. Its a library for showing Live Cricket Score, Cricket Schedule and Statistics',
+    author = 'Roanuz Softwares Private Ltd',
+    author_email = 'contact@roanuz.com',
+    url = 'https://github.com/roanuz/py-cricket',
+    package_dir={'': 'src'},
+    packages=[''],
     install_requires=['requests==2.5.1'],
     entry_points='''
         [console_scripts]
-        pycricket=pycricket:RcaAppApp
+        pycricket=src.pycricket:RcaApp
     ''',
 )
