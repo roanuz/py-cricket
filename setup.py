976f1b7727723084799bab372d5887df5ac4455a
Rajeev Raju <rajeev.r@roanuz.com>
2016-04-13 15:32:37 +0530

    src package has updated

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 
 setup(
     name = 'py-cricket',
-    version = '0.1.0',
+    version = '1.0.1',
     description = 'Easy to install and simple way to access all Roanuz Cricket APIs. Its a library for showing Live Cricket Score, Cricket Schedule and Statistics',
     author = 'Roanuz Softwares Private Ltd',
     author_email = 'contact@roanuz.com',
-    url = 'https://github.com/roanuz/py-cricket', 
-    packages=['rcapycricket'],
+    url = 'https://github.com/roanuz/py-cricket',
+    package_dir={'': 'src'},
+    packages=[''],
     install_requires=['requests==2.5.1'],
     entry_points='''
         [console_scripts]
-        pycricket=pycricket:RcaAppApp
+        pycricket=src.pycricket:RcaApp
     ''',
 )
-
